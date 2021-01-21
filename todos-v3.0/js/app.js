4ef0b6967bef033922c9f911345d79ecf79430be
ahnanne <54733637+ahnanne@users.noreply.github.com>
2021-01-21 18:59:55 +0900

    Todos v3.0 실습 중간 커밋

---
+++ todos-v3.0/js/app.js
@@ -0,0 +1,78 @@
+// global
+let todos = [];
+
+// 💚 요소 노드 취득 모음
+const $todos = document.querySelector('ul.todos');
+const $input = document.querySelector('.input-todo');
+
+// 💚 이벤트 핸들러 모음
+// 렌더링
+const render = () => {
+  let html = '';
+
+  todos.forEach(todo => {
+    html += `<li id="${todo.id}" class="todo-item">
+    <input id="ck-${todo.id}" class="checkbox" type="checkbox" ${todo.completed ? 'checked' : ''}/>
+    <label for="ck-${todo.id}">${todo.content}</label>
+    <i class="remove-todo far fa-times-circle"></i>
+  </li>
+  `;
+
+    $todos.innerHTML = html;
+  });
+};
+
+// 데이터를 ID순으로 정렬
+const sortData = () => {
+  todos.sort((todo1, todo2) => todo2.id - todo1.id);
+};
+
+// 가장 먼저 데이터 fetch 해오기
+const fetchTodos = () => {
+  // TODO: 데이터 취득(잠정 처리)
+  todos = [
+    { id: 1, content: "HTML", completed: false },
+    { id: 3, content: "JavaScript", completed: false },
+    { id: 2, content: "CSS", completed: false }
+  ];
+
+  sortData();
+
+  render();
+};
+
+// 새로운 ID 생성 함수
+const generateId = () => {
+  let maxId = -Infinity;
+
+  if (todos[0] === undefined) return 1;
+  todos.forEach(todo => {
+    if (todo.id > maxId) maxId = todo.id;
+  });
+
+  return maxId + 1;
+};
+
+// 새로운 todo 추가하기
+const addTodo = newTodo => {
+  todos = [
+    ...todos,
+    { id: generateId(), content: newTodo, completed: false }
+  ];
+
+  sortData();
+
+  render();
+};
+
+// 💚 이벤트 핸들러 등록 모음
+// 가장 먼저 데이터 fetch 해오기
+document.addEventListener('DOMContentLoaded', fetchTodos);
+
+// 새로운 todo 추가하기
+$input.onkeyup = e => {
+  if (e.key !== 'Enter') return;
+
+  addTodo(e.target.value);
+  e.target.value = '';
+};
